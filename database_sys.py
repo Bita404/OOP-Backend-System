0d384af0031fa72c4311accf5bce7fd4bccc7f84
Bita <87811454+Bita404@users.noreply.github.com>
2025-01-05 19:09:11 +0330

    Add files via upload

--- database_sys.py
+++ database_sys.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import  pandas as pd
 import matplotlib.pyplot as plt
+import logging
 
 class DB_connection :
      def __init__(self, user, password, host, database):
@@ -17,3 +18,26 @@ class DB_connection :
           host = self.host,
           database = self.databse
         )   
+        
+     def execute_query(self, query, data=None, 
+                      fetch=False):
+        try:
+            self.connnection() 
+            mycursor = self.connect.cursor() 
+            mycursor.execute(query, data)
+        except Exception as e:
+            print(e)
+        else:
+            if fetch:
+                data = mycursor.fetchall()
+                return data
+            affected_rows = mycursor.rowcount
+            self.connect.commit()
+            return affected_rows
+        finally:
+            if self.connect.is_connected():
+                mycursor.close()
+                self.connect.close()   
+                
+class                 
+                
